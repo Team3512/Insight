99ef4be9aba8e0d144b3f2ff515d459fbc7033d8
Tyler Veness <calcmogul@gmail.com>
2014-02-28 19:30:43 -0800

    Added error abstraction to mjpeg_sck

--- src/MJPEG/mjpeg_sck.c
+++ src/MJPEG/mjpeg_sck.c
@@ -46,6 +46,40 @@ mjpeg_sck_setnonblocking(mjpeg_socket_t sd, int enable)
     return 0;
 }
 
+mjpeg_sck_status
+mjpeg_sck_geterror()
+{
+#ifdef _WIN32
+    switch (WSAGetLastError()) {
+        case WSAEWOULDBLOCK :  return SCK_NOTREADY;
+        case WSAECONNABORTED : return SCK_DISCONNECT;
+        case WSAECONNRESET :   return SCK_DISCONNECT;
+        case WSAETIMEDOUT :    return SCK_DISCONNECT;
+        case WSAENETRESET :    return SCK_DISCONNECT;
+        case WSAENOTCONN :     return SCK_DISCONNECT;
+        default :              return SCK_ERROR;
+    }
+#else
+    // The followings are sometimes equal to EWOULDBLOCK,
+    // so we have to make a special case for them in order
+    // to avoid having double values in the switch case
+    if ((errno == EAGAIN) || (errno == EINPROGRESS)) {
+        return SCK_NOTREADY;
+    }
+
+    switch (errno) {
+        case EWOULDBLOCK :  return SCK_NOTREADY;
+        case ECONNABORTED : return SCK_DISCONNECT;
+        case ECONNRESET :   return SCK_DISCONNECT;
+        case ETIMEDOUT :    return SCK_DISCONNECT;
+        case ENETRESET :    return SCK_DISCONNECT;
+        case ENOTCONN :     return SCK_DISCONNECT;
+        case EPIPE :        return SCK_DISCONNECT;
+        default :           return SCK_ERROR;
+    }
+#endif
+}
+
 int
 mjpeg_sck_close(mjpeg_socket_t sd)
 {
